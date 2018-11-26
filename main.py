2cf57ede92837b486e7fcc0ac86515213cea3f89
nkhachan <noopur.khachane@gmail.com>
2018-11-25 18:47:04 -0800

    add registration page

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask import render_template
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 from flask_pymongo import PyMongo
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
@@ -13,11 +14,21 @@ app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['MONGO_DBNAME'] = 'website'
 app.config['MONGO_URI'] = 'mongodb://nkhachan:something65@ds056419.mlab.com:56419/website'
 mongo = PyMongo(app)
+user = mongo.db.users
+
+
+class RegisterForm(FlaskForm):
+   username   = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
+   email      = StringField('Email', validators=[DataRequired()], render_kw={"placeholder": "Email"})
+   password   = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
+   confirmpwd = PasswordField('Confirm Password', validators=[DataRequired()], render_kw={"placeholder": "Confirm Password"})
+   register   = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-   username = StringField('Username', validators=[DataRequired()])
-   password = PasswordField('Password', validators=[DataRequired()])
+   username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
+   password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
    submit   = SubmitField('Sign In')
+
 
 @app.route('/')
 def index():
@@ -25,7 +36,7 @@ def index():
 
 
 @app.route("/login", methods=['GET'])
-def loginshow():
+def showlogin():
    form = LoginForm()
    return render_template('login.html', title="Login", form=form)
 
@@ -35,6 +46,28 @@ def login():
    form = LoginForm()
    username = request.form['username']
    password = request.form['password']
-   user = mongo.db.users
-   user.insert({'username': username, 'password': password})
+   query = user.find({"$and":[ {"username":username}, {"password":password}]})
+   if (query.count() == 1):
+      print()
    return render_template('login.html', title="Login", form=form)
+
+
+@app.route("/register", methods=['GET'])
+def show_register():
+   form = RegisterForm()
+   return render_template('register.html', title="Register", form=form)
+
+@app.route("/register", methods=['POST'])
+def register():
+   form = RegisterForm()
+
+   username   = request.form['username']
+   password   = request.form['password']
+   confirmpwd = request.form['confirmpwd']
+   email      = request.form['email']
+
+   if (confirmpwd == password):
+      user.insert({'username': username, 'password': password, 'email': email})
+
+   return render_template('register.html', title="Register", form=form)
+
