aa53add4a4e98810b1d7ed30d5d47cc333cb175f
nkhachan <noopur.khachane@gmail.com>
2019-01-15 15:48:38 -0800

    fix errors

--- APIs/Yelp.py
+++ APIs/Yelp.py
@@ -2,6 +2,7 @@ from yelpapi import YelpAPI
 
 
 def findlocalRestaurants(lat, lng):
+    pass
     #yelp_api = YelpAPI(apikey)
     #search_results = yelp_api.search_query(latitude=lat, longitude=lng, categories="Restaurants", radius=8000)
     #return search_results
